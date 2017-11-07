685e61fec8a424d6c0765986b35dbf39aac6b972
Jeffrey Hinzmann <JAHinzmann@gmail.com>
2017-11-07 18:45:28 -0500

    Ben helped me get started

---
+++ assets/javascript/hangman.js
@@ -0,0 +1,12 @@
+/* Stuff I need:
+
+1. An Array of words (word bank)
+2. An array of strings:
+a. Take the word, and make it an array of letters (an empty array for the target word, perhaps?)
+b. Check the letters in the array against the letter entered by the player
+c. Make sure to classify the entry as either in or not in the array (think about what operators will be needed to do this)
+3. Letter entry
+4. Letter Guess
+5. Number of guesses remaining
+6. win/loss record
+*/
