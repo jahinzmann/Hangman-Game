7db914894ca23bf3d5389d8a95f1b5c2a76b10c7
Jeff Hinzmann <JAHinzmann@gmail.com>
2017-11-08 23:58:21 -0500

    Improved with tips and extensive tutoring assistance, incl debugging

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -15,50 +15,49 @@ To use later:
 Math.floor(x)
 math.random(x) (choose a random word with a for-loop)
 
+Wrap shit in functions!
 */
 
-var words = ["Westworld", "The Good The Bad and the Ugly", "The Wild Bunch", "The Searchers"
+var words = ["Westworld", "The Good The Bad and the Ugly", "The Wild Bunch", "The Searchers", "Unforgiven", "True Grit"];
 
-  ];
 var word = words[Math.floor(Math.random() * words.length)];
-       
+console.log("word: ", word);   
+
 var answerArray = [];
 
 for (var i = 0; i < word.length; i++) {
     
-        answerArray[i] = "";
+  answerArray[i] = "";
     
-      }
-    
-    var remainingLetters = word.length;
+}
+console.log("answerArray: ", answerArray);   
+var remainingLetters = word.length;
 
-    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-    
-    if (guess === null) {
-    
-        break;
-    
-     } else if (guess.length !== 1) {
-    
-        alert("Please enter a single letter.");
-    
-      } else {
-    
-        for (var j = 0; j < word.length; j++) {
-            
-            if (word[j] === guess) {
-            
-                answerArray[j] = guess;
-            
-                remainingLetters--;
-            
-                }
-            
-              }
-    
-      }      
+//var guess = prompt("Guess a letter, or click Cancel to stop playing.");
 
-        document.addEventListener("DOMContentLoaded", function(event) {
+if (guess === null) {
+
+} else if (guess.length !== 1) {
+
+  alert("Please enter a single letter.");
+
+} else {
+
+  for (var j = 0; j < word.length; j++) {
+      
+    if (word[j] === guess) {
+    
+      answerArray[j] = guess;
+  
+      remainingLetters--;
+    
+    }
+      
+  }
+
+}      
+
+        /*document.addEventListener("DOMContentLoaded", function(event) {
             var MorriconeGoodBadUgly = new Audio('/assets/sounds/Ennio_Morricone-GoodBadUgly.ogg');
-            MorriconeGoodBadUgly.play();
+            MorriconeGoodBadUgly.play();*/
 
